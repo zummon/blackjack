cd037b70acc8b96a0d807deb1a16550f05351682
zummon <zummon.space@gmail.com>
2024-06-18 23:00:56 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+let symbols = ['♠️','♥️','♦️','♣️']
+let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+let faces = ['A',...values.slice(1,-3),'J','Q','K']
+let cards = symbols.flatMap((symbol) => faces.map((face) => face + symbol))
+</script>
+
+<div class="flex flex-wrap justify-center gap-4 text-center">
+{#each cards as card}
+<div class="">
+{card}
+</div>
+{/each}
+</div>
