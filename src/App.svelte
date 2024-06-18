b6b3abf9716cde64d4a4135668019f8ce1a7e56d
zummon <zummon.space@gmail.com>
2024-06-18 23:29:44 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
-let symbols = ['♠️','♥️','♦️','♣️']
-let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
-let faces = ['A',...values.slice(1,-3),'J','Q','K']
-let cards = symbols.flatMap((symbol) => faces.map((face) => face + symbol))
+  let symbols = ["♠️", "♥️", "♦️", "♣️"];
+  let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+  let faces = ["A", ...values.slice(1, -3), "J", "Q", "K"];
+  let cards = symbols.flatMap((symbol) => faces.map((face) => face + symbol));
 </script>
 
 <div class="flex flex-wrap justify-center gap-4 text-center">
-{#each cards as card}
-<div class="">
-{card}
-</div>
-{/each}
+  {#each cards as card}
+    <div class="">
+      {card}
+    </div>
+  {/each}
 </div>
