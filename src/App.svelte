d8702536f78e78996a2494d875c775e3580ca9e8
zummon <zummon.space@gmail.com>
2024-06-19 19:56:50 +0700

    try

--- src/App.svelte
+++ src/App.svelte
@@ -24,32 +24,42 @@
   });
 </script>
 
-<div class="flex flex-wrap justify-center gap-4 text-center">
+<div class="flex flex-wrap justify-center items-baseline gap-4 text-center">
   <div class="">
     <button
-      class="text-blue-500"
+      class="text-blue-300 text-4xl"
       onclick={() => {
         restart();
-      }}>[o]</button
+      }}>↻</button
     >
   </div>
   {#each opponents as opponent, index}
-    <div class="flex flex-col gap-2 border border-gray-500 px-2">
+    <div class="flex flex-col gap-2 px-2">
       <div class="">
-        <span class="">{index + 1}</span>
-        {#if index > 0}
+        <span class="">Player {index + 1}</span>
+        {#if index > 1}
           <button
-            class="text-red-500"
+            class="text-red-500 text-2xl"
             onclick={() => {
+              opponents.forEach((opponent) => {
+                opponent.forEach((card) => {
+                  cards.push(card);
+                });
+              });
+
               opponents.splice(index, 1);
-            }}>[x]</button
+            }}>✘</button
           >
         {/if}
       </div>
       {#each opponent as card}
-        <div class="" class:text-red-500={card.red}>
-          <span class="font-bold text-2xl">{card.face}</span>
-          <span class="text-4xl">{card.symbol}</span>
+        <div
+          class="bg-white flex gap-1 rounded pl-2 pr-2 pb-2"
+          class:text-red-500={card.red}
+          class:text-black={!card.red}
+        >
+          <div class="font-bold text-2xl">{card.face}</div>
+          <div class="text-4xl">{card.symbol}</div>
         </div>
       {/each}
     </div>
@@ -57,11 +67,12 @@
   {#if opponents.length < 25}
     <div class="">
       <button
-        class="text-blue-500"
+        class="text-blue-300 text-4xl"
         onclick={() => {
           opponents.push([]);
-          restart();
-        }}>[+]</button
+          opponents[opponents.length - 1] = [cards.pop()];
+          opponents[opponents.length - 1].push(cards.pop());
+        }}>✚</button
       >
     </div>
   {/if}
