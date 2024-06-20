753405cfbcbb1d17e6b732494d1b1323e829c798
zummon <zummon.space@gmail.com>
2024-06-20 07:11:24 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import { onMount } from "svelte";
 
   const symbols = ["♦️", "♣️", "♥️", "♠️"];
-  const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
   const faces = ["A", ...values.slice(1, -3), "J", "Q", "K"];
   const allCards = symbols.flatMap((symbol, idx) =>
     faces.map((face, index) => ({ value: values[index], face: face, symbol: symbol, red: idx % 2 == 0 }))
@@ -14,8 +14,7 @@
   function restart() {
     cards = allCards.toSorted(() => Math.random() - 0.5);
     opponents.forEach((opponent, index) => {
-      opponents[index] = [cards.pop()];
-      opponents[index].push(cards.pop());
+      opponents[index] = [cards.pop(), cards.pop()];
     });
   }
 
@@ -24,29 +23,18 @@
   });
 </script>
 
-<div class="flex flex-wrap justify-center items-baseline gap-4 text-center">
-  <div class="">
-    <button
-      class="text-blue-300 text-4xl"
-      onclick={() => {
-        restart();
-      }}>↻</button
-    >
-  </div>
+<div class="flex flex-wrap justify-center items-center gap-4 text-center py-2">
   {#each opponents as opponent, index}
     <div class="flex flex-col gap-2 px-2">
       <div class="">
-        <span class="">Player {index + 1}</span>
+        <span class="leading-8 font-semibold">Player {index + 1}</span>
         {#if index > 1}
           <button
             class="text-red-500 text-2xl"
             onclick={() => {
-              opponents.forEach((opponent) => {
-                opponent.forEach((card) => {
-                  cards.push(card);
-                });
+              opponents[index].forEach((card) => {
+                cards.push(card);
               });
-
               opponents.splice(index, 1);
             }}>✘</button
           >
@@ -54,24 +42,43 @@
       </div>
       {#each opponent as card}
         <div
-          class="bg-white flex gap-1 rounded pl-2 pr-2 pb-2"
+          class="bg-white rounded pl-2 pr-2 pb-2 whitespace-nowrap"
           class:text-red-500={card.red}
           class:text-black={!card.red}
         >
-          <div class="font-bold text-2xl">{card.face}</div>
-          <div class="text-4xl">{card.symbol}</div>
+          <span class="font-bold text-2xl">{card.face}</span>
+          <span class="text-4xl align-top">{card.symbol}</span>
         </div>
       {/each}
+      <div class="">
+        <span class="font-semibold">
+          ({opponent.reduce((prev, curr) => {
+            return prev + curr.value;
+          }, 0)})
+        </span>
+        <button
+          class="text-2xl align-top"
+          onclick={() => {
+            opponents[index].push(cards.pop());
+          }}>✚</button
+        >
+      </div>
     </div>
   {/each}
   {#if opponents.length < 25}
     <div class="">
+      <div class="">
+        <button
+          class="text-4xl"
+          onclick={() => {
+            restart();
+          }}>↻</button
+        >
+      </div>
       <button
-        class="text-blue-300 text-4xl"
+        class="text-4xl"
         onclick={() => {
-          opponents.push([]);
-          opponents[opponents.length - 1] = [cards.pop()];
-          opponents[opponents.length - 1].push(cards.pop());
+          opponents.push([cards.pop(), cards.pop()]);
         }}>✚</button
       >
     </div>
